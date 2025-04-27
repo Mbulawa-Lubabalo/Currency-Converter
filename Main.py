bccb63f773bc9de93cc957a2c212e9254f1829a8
Lubabalo Mbulawa <lumbulacpt024@student.wethinkcode.co.za>
2025-04-27 17:13:42 +0200

    Added an icon image on the window

---
+++ Main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+window = Tk()
+window.geometry("420x420")
+window.title("Currrency Converter")
+
+icon = PhotoImage(file='IconImage\currencyIcon.png')
+window.iconphoto(True,icon)
+
+
+window.mainloop()
