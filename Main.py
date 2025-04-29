f4c17264162339efd14dfc48e318f57c520c5f6c
Lubabalo Mbulawa <lumbulacpt024@student.wethinkcode.co.za>
2025-04-29 18:25:46 +0200

    working on styling the window

--- Main.py
+++ Main.py
@@ -10,29 +10,41 @@ window.config(background="#c1c4c7")
 
 header = Label(window, 
                text="Currency Converter", 
-               background="#c1c4c7").grid(row=0, column=0)
+               background="#c1c4c7", 
+               font=("Arial", 16)
+               ).grid(row=0, column=0, columnspan=2, pady=10)
 # header.place(x=0, y=0)
 
 # Input Fields
 fromCurrency_label = Label(window, text="From:")
-fromCurrency_label.grid(row=2, column=0)
+fromCurrency_label.grid(row=1, column=0, padx=10, pady=5)
 # fromCurrency_entry = Entry(window)
 currencies = ["USD", "EUR", "GBP", "INR", "AUD"]
 defaultOption = StringVar(value="USD")
-fromCurrency_entry = OptionMenu(window, defaultOption, *currencies)
-fromCurrency_entry.grid(row=3, column=0)
+fromCurrency_menu = OptionMenu(window, defaultOption, *currencies)
+fromCurrency_menu.grid(row=3, column=0, padx=10, pady=5)
 
 toCurrency_label = Label(window, text="To:")
-toCurrency_label.grid(row=2, column=1)
+toCurrency_label.grid(row=1, column=1, padx=10, pady=5)
 toCurrency_entry = OptionMenu(window, defaultOption, *currencies)
-toCurrency_entry.grid(row=3, column=1)
+toCurrency_entry.grid(row=2, column=1, padx=10, pady=5)
 
 # Amount
-amount_label = Label(window, text="Amount:").grid(row=4)
-amount_entry = Entry(window).grid(row=5)
+amount_label = Label(window, text="Amount:").grid(row=3, column=0, padx=10, pady=5)
+amount_entry = Entry(window).grid(row=4, column=0, padx=10, pady=5)
+
+# convertedValue_label = Label(window, text="converted", background="#ffffff").grid(row=6)
 
 # Convert button
-convertButton = Button(window, text="Convert").grid(row=6)
+convertButton = Button(window, 
+                       text="Convert", 
+                       ).grid(row=5, column=0, columnspan=2, pady=10)
+
+# Result Label
+result_label = Label(window, 
+                     text="Converted Amount: ", 
+                     background="#c1c4c7"
+                     ).grid(row=6, column=0, columnspan=2, pady=10)
 
 
 
